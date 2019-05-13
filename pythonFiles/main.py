3699ec75f2f9cb8a0084574a1efbba78af9420fa
hreidar17 <hreidar17@ru.is>
2019-05-13 14:36:20 +0000

    using layout file

--- pythonFiles/main.py
+++ pythonFiles/main.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 @app.route("/twoPlayer")
 def TwoPlayer():
-    return render_template('twoPlayer.html')
+    return render_template('twoPlayer.html', title='P vs P')
 
 @app.route("/playerVsComputer")
 def playerVsComputer():
-    return render_template('playerVsComputer.html')
+    return render_template('playerVsComputer.html', title='P vs Ai')
 
 if __name__ == '__main__':
     app.run(debug=True)
