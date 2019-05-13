b1432b4c536a3163f1d9f32fc06d0790edc55871
Benedikt Runar <benedikt17@ru.is>
2019-05-13 15:48:29 +0000

    remove files that were added before they were put in the gitignore file. Syntax editing in main

--- pythonFiles/main.py
+++ pythonFiles/main.py
@@ -2,19 +2,23 @@ from flask import Flask, render_template
 from letters import *
 app = Flask(__name__)
 
+
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/twoPlayer")
 def TwoPlayer():
     return render_template('twoPlayer.html')
+
 
 @app.route("/playerVsComputer")
 def playerVsComputer():
     lettersChosen = letters()
     return render_template('playerVsComputer.html', letters = lettersChosen)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
