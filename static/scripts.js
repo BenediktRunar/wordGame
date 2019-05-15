9c1289cf2f7b2bcb548109874bf37b0f91417ffd
hreidar17 <hreidar17@ru.is>
2019-05-15 16:27:24 +0000

    more jquery goodness

--- static/scripts.js
+++ static/scripts.js
@@ -3,11 +3,53 @@ $(document).ready(function() {
     console.log("script connected");
 
     var strLen = 0;
+    var strOut = "";
+
+    var gameField = $('div.gameContainer').find('div.box1');
+    var gameBoard = gameField.find()
+    var outputField = gameField.find('span.output');
+
 
     $('.letterBox').on('click', function(event) {
 
         var squareSelected = $(this);
-        console.log(squareSelected.find('h1')[0].innerText);
+        var letterInBox = squareSelected.find('h1')[0].innerText;
+        console.log(letterInBox);
+        strLen++;
+        strOut += letterInBox;
+        let letter = document.createElement("div");
+        letter.className = "chosenLetter";
+        letter.textContent = letterInBox;
+        changeOut();
+        letter.onclick = (e) => {
+
+            var thisLetter = $(e);
+            //console.log("blablabla: " + e);
+            //console.log(gameField);
+            console.log(thisLetter);
+            var childIndex = 0;
+
+            console.log(e.target);
+            $(e.target).remove();
+            $(thisLetter).remove();
+            strLen--;
+            changeOut();
+            /*
+            * var i = 0;
+                while( (child = child.previousSibling) != null )
+                  i++;
+                //at the end i will contain the index.
+            * */
+        };
+        gameField.append(letter);
     });
+
+    function changeOut(){
+        //console.log(outputField);
+        //console.log(strLen + ", " +strOut);
+        outputField[0].innerHTML = strLen + ", \"" +strOut+"\"";
+    }
+
+
 
 });
