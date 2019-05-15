0b14d7117d813591c619780e74fc70b9d222e0c3
hreidar17 <hreidar17@ru.is>
2019-05-15 21:43:11 +0000

    code cleanup

--- static/scripts.js
+++ static/scripts.js
@@ -1,52 +1,48 @@
 $(document).ready(function() {
-    var hre = document.getElementById("submit")
-    var baseUrl = hre.getAttribute('href')
-    
-    console.log(baseUrl)
-    console.log("script connected");
-
+    var hre = document.getElementById("submit");
+    var baseUrl = hre.getAttribute('href');
+    var gameField = $('div.gameContainer').find('div.box1');
     var strLen = 0;
     var strOut = "";
 
-    var gameField = $('div.gameContainer').find('div.box1');
-    var gameBoard = gameField.find('div.gameBoard');
-    var outputField = gameField.find('span.output');
-
-    
+    console.log(baseUrl)
+    console.log("script connected");
 
     $('.letterBox').on('click', function(event) {
-        var squareSelected = $(this);
-        var letterInBox = squareSelected.find('h1')[0].innerText;
+
+        var letterInBox = $(this).find('h1')[0].innerText;
+
         console.log(letterInBox);
-        strLen++;
         strOut += letterInBox;
         let letter = document.createElement("div");
         letter.className = "chosenLetter";
         letter.textContent = letterInBox;
-        changeOut();
-        squareSelected = event.currentTarget;
+        changeOut(1);
+
+        var squareSelected = event.currentTarget;
         squareSelected.setAttribute("style", "visibility: hidden");
 
         letter.onclick = (e) => {
             var childIndex = -1;
-            var thisChild = e.target;
-            while((thisChild = thisChild.previousSibling) != null) {childIndex++;}
+            for(var child = e.target; child.previousSibling != null; child = child.previousSibling){
+                childIndex++;
+            }
             squareSelected.setAttribute("style", "visibility: block");
-            console.log(childIndex);
             strOut = strOut.slice(0, childIndex) + strOut.slice(childIndex+1);
             $(e.target).remove();
-            strLen--;
-            changeOut();
+            changeOut(-1);
         };
 
-        gameBoard.append(letter);
+        gameField.find('div.gameBoard').append(letter);
 
-        console.log("base:" + baseUrl,strOut)
+        console.log("base:" + baseUrl,strOut);
+
         hre.setAttribute("href", baseUrl + strOut)
     });
     
-    function changeOut(){
-        outputField[0].innerHTML = strLen + ", \"" +strOut+"\"";
+    function changeOut(n){
+        strLen += n;
+        gameField.find('span.output')[0].innerHTML = strLen + ", \"" +strOut+"\"";
     }
 
     function getOutput(){
