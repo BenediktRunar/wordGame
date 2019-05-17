07f418c218a9c36c05d93927406ce12cad45a18a
hreidar17 <hreidar17@ru.is>
2019-05-17 14:22:09 +0000

    merging with master

--- static/scripts.js
+++ static/scripts.js
@@ -1,40 +1,12 @@
-var scoreList = [];
-var wordList = [];
-
-
-function printScore(input, word){
-    word = new URL(word);
-    var lastWord = word.searchParams.get("userWord");
-    console.log('score was: ' + input + ", "+lastWord);
-    wordList.push(lastWord);
-    console.log(wordList);
-
-    //var scoreField = $('div.gameContainer').find('div.box1').find('table.gameScore');
-
-    var wordField = $('div.gameContainer').find('div.scoreBoard');
-    console.log(wordField);
-
-    if (localStorage.getItem('words') == null){
-        wordList.push(lastWord);
-    } else {
-        console.log(localStorage.getItem('words'));
-    }
-    localStorage.setItem( 'words', wordList );
-    console.log(wordList);
-
-    //var newWord = document.createElement('DIV');
-    //newWord.innerHTML = lastWord;
-    //console.log(newWord);
-    //wordField.append(newWord);
-}
-
 $(document).ready(function() {
     //Get existing url
-    var test = document.getElementById("test")
+    var test = document.getElementById("test");
     var gameField = $('div.gameContainer').find('div.box1');
     var strOut = "";
-    var strLen = 0;
 
+    $('.logoContainer').on('click', function(event) {
+
+    });
 
     $('.letterBox').on('click', function(event) {
 
@@ -45,7 +17,6 @@ $(document).ready(function() {
         let letter = document.createElement("div");
         letter.className = "chosenLetter";
         letter.textContent = letterInBox;
-        changeOut(1);
 
         var squareSelected = event.currentTarget;
         squareSelected.setAttribute("style", "visibility: hidden");
@@ -58,28 +29,13 @@ $(document).ready(function() {
             squareSelected.setAttribute("style", "visibility: block");
             strOut = strOut.slice(0, childIndex) + strOut.slice(childIndex+1);
             $(e.target).remove();
-            changeOut(-1);
         };
 
         gameField.find('div.gameBoard').append(letter);
-
         // changing the href attrinbute
-        test.setAttribute("value", strOut)
-        console.log(test)
+        test.setAttribute("value", strOut);
         //console.log("here: " + hre.getAttribute('href'))
     });
-
-    function changeOut(n){
-        strLen += n;
-    }
-
-    function getOutput(){
-        return strOut;
-    }
-
-    function test(){
-        console.log('function within ready scope');
-    }
 
 });
 
