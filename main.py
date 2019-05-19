75b6d23414bccfc18f8dddebdcf0ad9b7061f8ec
Benedikt Runar <benedikt17@ru.is>
2019-05-19 01:15:12 +0000

    df

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
+import os
 from letters import *
 from computersTurn import *
 from game import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 
 # home site
 @app.route("/")
