0dc637e60b92ee2f7c77fc5da81631f3cc02ef47
Benedikt Runar <benedikt17@ru.is>
2019-05-19 07:26:45 +0000

    boilerplate

--- LetterSquare.py
+++
@@ -1,5 +0,0 @@
-from scoreOfLetters import *
-class Square:
-    def __init__(self, letter):
-        self.letter = letter
-        self.value = scoreOfLetter(letter)
--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .letters import *
-from .computersTurn import *
